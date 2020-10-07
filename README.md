47f8e2990a9a9d9a48cf388b4689f08bb2b23c5e
Jose Morales <jmorales072095@gmail.com>
2020-10-06 21:47:43 -0700

    added files and code

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-# personnel-cards
+## Personnel-Cards
+    
+    
+## Description
+1. Creating html templates and using constructor to build employees card 
+2. Answer a few questions after running inquirer to get peronnel cards
+3. It has an option of Manager, Engenieer and Intern 
+4. 
+ 
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Test](#test)
+* [Contributors](#contributors)
+* [Questions](#questions)
+
+## Installation
+install Inquirier in vs code
+ ## Usage
+to create cxards for various employees by answering a  series of questions made by inquirer, after answering question users will be prenesnted with a personalized card for each employee
+    
+## License
+Lincenses used: MIT
+## Test
+have vs code installed in computer and installing inquirer
+## Contributors
+Jose Morales
+## Questions
+GitHub: https://github.com/jmoarles072095
+
+Contact Me: jmorales072095@gmail.com
+    
--- app.js
+++ app.js
@@ -9,27 +9,107 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const Employee = require("./lib/Employee");
 
+// Create team array
+const team = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+//Prompt the user questions about personnel roles
+function addEmployee() {
+    inquirer.prompt([{
+            type: "input",
+            message: "What is your employee name?",
+            name: "name",
+        },
+        {
+            type: "input",
+            message: "What is your employee id number?",
+            name: "id",
+        },
+        {
+            type: "input",
+            message: "What is your employee email?",
+            name: "email",
+        },
+        {
+            type: "list",
+            message: "What is the role of the employee?",
+            name: "role",
+            choices: ["Manager", "Engineer", "Intern", ]
+        },
+    ]).then(function(response) {
+        const employees = new Employee(response.name, response.id, response.email);
+        // If role is manager = Enter office number
+        if (response.role === "Manager") {
+            inquirer.prompt([{
+                type: "input",
+                message: "What is your office number?",
+                name: "officeNumber",
+            }, ]).then(function(response) {
+                // Creates new manager
+                const manager = new Manager(employees.name, employees.id, employees.email, response.officeNumber);
+                team.push(manager);
+                addNewEmployee();
+            });
+            // If role is engineer = Enter github username
+        } else if (response.role === "Engineer") {
+            inquirer.prompt([{
+                    type: "input",
+                    message: "What is your Github username?",
+                    name: "github",
+                }, ]).then(function(response) {
+                    // Creates new engineer
+                    const engineer = new Engineer(employees.name, employees.id, employees.email, response.github);
+                    team.push(engineer);
+                    addNewEmployee();
+                })
+                // If role is intern = Enter school
+        } else if (response.role === "Intern") {
+            inquirer.prompt([{
+                type: "input",
+                message: "Which school did you attend?",
+                name: "school",
+            }, ]).then(function(response) {
+                // Creates new intern
+                const intern = new Intern(employees.name, employees.id, employees.email, response.school);
+                team.push(intern);
+                addNewEmployee();
+            });
+        };
+    });
+};
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+// Function to add more personnel
+function addNewEmployee() {
+    inquirer.prompt([{
+        type: "list",
+        message: "Would you like to add another employee?",
+        name: "question",
+        choices: ["Yes", "No"],
+    }, ]).then(function(response) {
+        if (response.question === "No") {
+            const create = render(team);
+            createHTML(create);
+        } else {
+            addEmployee();
+        };
+    });
+};
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+// Function to create output folder and html file with personnel cards
+function createHTML(data) {
+    fs.mkdir("./output", function(err) {
+        if (err) {
+            throw err;
+        };
+    });
+    fs.writeFile(outputPath, data, function(err) {
+        if (err) {
+            throw err;
+        } else {
+            console.log("Success, you've created your team profile!");
+        };
+    });
+};
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+addEmployee();
